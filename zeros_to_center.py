f2637e188a5fd5d3ef55cadc3a50468a324e1b66
Deepraj Darbar <47897409+DeeprajDarbar@users.noreply.github.com>
2020-09-15 17:12:33 -0400

    Update zeros_to_center.py
    Added a compact code by use of filter, lambda, and NumPy zeros to remove for loop

--- zeros_to_center.py
+++ zeros_to_center.py
@@ -1,14 +1,12 @@
+import numpy as np
 
 def zerostocentre(arr):
-    '''
-    a Fucntion to move all zeros to center of an array
-    '''
-    n = arr.count(0)
-    a = []
-    for _ in range(n):
-        arr.remove(0)
-        a.append(0)
-    return arr[:len(arr)//2] + a + arr[len(arr)//2:]
+  '''
+  a Fucntion to move all zeros to center of an array
+  '''
+  n = arr.count(0)
+  arr = list(filter(lambda a: a != 0, arr))
+  return arr[:len(arr)//2] + list(np.zeros(n, dtype=int)) + arr[len(arr)//2:]
 
 
 arr = [0,0,1,2,3,4,5,6,0,0,0]
